26cc1489cf967135045a0f96ec24b0a9ae2d0ce5
avollkopf <43980694+avollkopf@users.noreply.github.com>
2021-02-11 21:56:35 +0100

    Update setup.py
    Add RPi.GPIO to setup.py for rasperrypi platforms

--- setup.py
+++ setup.py
@@ -1,5 +1,31 @@
 from setuptools import setup, find_packages
 from cbpi import __version__
+import platform
+
+INSTALL_REQUIRES= [
+          "aiohttp==3.7.3",
+          "aiohttp-auth==0.1.1",
+          "aiohttp-route-decorator==0.1.4",
+          "aiohttp-security==0.4.0",
+          "aiohttp-session==2.9.0",
+          "aiohttp-swagger==1.0.15",
+          "aiojobs==0.3.0",
+          "aiosqlite==0.16.0",
+          "cryptography==3.3.2",
+          "requests==2.25.1",
+          "voluptuous==0.12.1",
+          "pyfiglet==0.8.post1",
+          'pandas==1.1.5',
+          'click==7.1.2',
+          'shortuuid==1.0.1',
+          'tabulate==0.8.7',
+          'asyncio-mqtt',
+          'cbpi4ui',
+      ]
+
+if (platform.node()=="raspberrypi"):
+    INSTALL_REQUIRES.append("RPi.GPIO")
+
 
 setup(name='cbpi',
       version=__version__,
@@ -14,26 +40,8 @@ setup(name='cbpi',
       '': ['*.txt', '*.rst', '*.yaml'],
       'cbpi': ['*','*.txt', '*.rst', '*.yaml']},
 
-      install_requires=[
-          "aiohttp==3.7.3",
-          "aiohttp-auth==0.1.1",
-          "aiohttp-route-decorator==0.1.4",
-          "aiohttp-security==0.4.0",
-          "aiohttp-session==2.9.0",
-          "aiohttp-swagger==1.0.15",
-          "aiojobs==0.3.0",
-          "aiosqlite==0.16.0",
-          "cryptography==3.3.1",
-          "requests==2.25.1",
-          "voluptuous==0.12.1",
-          "pyfiglet==0.8.post1",
-          'pandas==1.1.5',
-          'click==7.1.2',
-          'shortuuid==1.0.1',
-          'tabulate==0.8.7',
-          'asyncio-mqtt',
-          'cbpi4-ui',
-      ],
+      install_requires=INSTALL_REQUIRES
+        ,
         dependency_links=[
         'https://testpypi.python.org/pypi',
         
