884f8b86f46ceda3abc024c582dd7e88be04e2c8
avollkopf <43980694+avollkopf@users.noreply.github.com>
2021-06-21 07:12:26 +0200

    Added particular numpy version
    existing version shows error when scipy gets installed

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(name='cbpi',
           'shortuuid==1.0.1',
           'tabulate==0.8.7',
           'asyncio-mqtt',
+          'numpy==1.20.3',
           'scipy',
           'cbpi4ui',
           'RPi.GPIO; sys_platform == "linux"'
