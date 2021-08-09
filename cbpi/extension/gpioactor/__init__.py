ec6498921efbe244d33a3d0c297333b9722f4a13
avollkopf <43980694+avollkopf@users.noreply.github.com>
2021-08-09 12:49:31 +0200

    Added gpio 28 & 29 to actors

--- cbpi/extension/gpioactor/__init__.py
+++ cbpi/extension/gpioactor/__init__.py
@@ -24,7 +24,7 @@ mode = GPIO.getmode()
 if (mode == None):
     GPIO.setmode(GPIO.BCM)
 
-@parameters([Property.Select(label="GPIO", options=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]), Property.Select(label="Inverted", options=["Yes", "No"],description="No: Active on high; Yes: Active on low")])
+@parameters([Property.Select(label="GPIO", options=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29]), Property.Select(label="Inverted", options=["Yes", "No"],description="No: Active on high; Yes: Active on low")])
 class GPIOActor(CBPiActor):
 
     @action(key="Cusotm Action", parameters=[Property.Number("Value", configurable=True), Property.Kettle("Kettle")])
@@ -70,7 +70,7 @@ class GPIOActor(CBPiActor):
             await asyncio.sleep(1)
             
 
-@parameters([Property.Select(label="GPIO", options=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]), Property.Number("Frequency", configurable=True)])
+@parameters([Property.Select(label="GPIO", options=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29]), Property.Number("Frequency", configurable=True)])
 class GPIOPWMActor(CBPiActor):
 
     # Custom property which can be configured by the user
